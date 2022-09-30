766cfaf97307b1dc387a77ede525a6f0ed7ed40d
Laskoru <hugo_burel@hotmail.com>
2022-09-30 16:01:46 +0200

    first commit

--- backend/routes/user.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const userCtrl = require("../controllers/user");
-const userRoutes = require("./routes/user");
-router.post("/src/app/auth/signup", userCtrl.signup);
-router.post("/", userCtrl.login);
-app.use("/api/auth", userRoutes);
-module.exports = router;
